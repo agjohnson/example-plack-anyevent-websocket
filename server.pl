ccd5af8ee50ad6dee31f7cdc0f7af1adfdf316c0
Anthony Johnson <aj@ohess.org>
2013-04-11 14:09:28 -0700

    Add example with SockJS and AnyEvent::Redis

--- server.pl
+++ server.pl
@@ -1,3 +1,4 @@
+
 use 5.010;
 use strict;
 use warnings;
@@ -6,10 +7,16 @@ use Plack::Builder;
 use Plack::Request;
 use Plack::Response;
 use Plack::Middleware::Static;
+use SockJS;
+use AnyEvent::Redis;
+use Data::Dumper;
+
+our $Guards = {};
 
 
 sub app {
     builder {
+        mount '/event' => websocket();
         enable 'Plack::Middleware::Static',
           path => qr/\.(?:html|js)$/,
           root => 'public/';
@@ -38,6 +45,37 @@ sub process {
             'Not found'
         )->finalize;
     }
+}
+
+sub websocket {
+    SockJS->new(
+        sockjs_url => '/sockjs-0.3.4.min.js',
+        chunked => 0,
+        handler => sub {
+            my ($session) = @_;
+
+            my $redis_sub = redis();
+            $Guards->{$redis_sub} = $redis_sub;
+            $redis_sub->subscribe('event', sub {
+                my ($data, $channel) = @_;
+                $session->write($data);
+            });
+
+            my $redis_pub = redis();
+            $Guards->{$redis_pub} = $redis_pub;
+            $session->on('data' => sub {
+                my ($session, $data) = @_;
+                $redis_pub->publish('event', $data);
+            });
+        }
+    );
+}
+
+sub redis {
+    AnyEvent::Redis->new(
+        host => '127.0.0.1',
+        port => 6379
+    );
 }
 
 app();
